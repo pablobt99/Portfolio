5d1c126fafb16c26751cd85b069dfa48c86cd736
pablo.toca <pablo.toca@alumnos.uneatlantico.es>
2020-05-10 14:47:45 +0200

    global selectors, html, styles

---
+++ Colors/app.js
@@ -0,0 +1,22 @@
+//Global selections
+const colorDivs = document.querySelectorAll('.color');
+const generateBtn = document.querySelector('.generate');
+const sliders = document.querySelectorAll('input[type = "range"]');
+const currentHexes = document.querySelectorAll('.color h2');
+
+//Functions
+/*
+function generateHex(){
+    const letters = '#0123456789ABCDEF';
+    let hash = '#';
+    for(let i = 0; i<6; i++){
+        hash += letters[Math.floor(Math.random()*16)];
+    }
+    return hash;
+}
+let randomHex = generateHex();
+console.log(randomHex);
+*/
+//lo de arrib es sin libreria con libraria es lo sigueinte
+
+
